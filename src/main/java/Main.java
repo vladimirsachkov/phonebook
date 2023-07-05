f949b720b5120a6c334705667b54c6844bc8d139
alexa <alexanderredar1@gmail.com>
2023-07-05 14:28:53 +0300

    Тестовая работа с hibernate.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
+import entity.PhonebookTableEntity;
 import org.hibernate.Session;
 import untils.HibernateUntil;
 
 public class Main {
     public static void main(String[] args) {
         Session session = HibernateUntil.getSessionFactory().openSession();
-        session.close();
+        session.beginTransaction();
+        PhonebookTableEntity list = session.get(PhonebookTableEntity.class, 2);
+        System.out.println(list.toString());
         HibernateUntil.shutdown();
     }
 }
