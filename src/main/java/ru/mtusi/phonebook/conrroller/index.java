b1951f6158cc700d13af7a6c87321b3a31ff609e
alexa <alexanderredar1@gmail.com>
2023-07-06 19:38:56 +0300

    Настройка MVC проекта.

--- src/main/java/ru/mtusi/phonebook/conrroller/index.java
+++ src/main/java/ru/mtusi/phonebook/conrroller/index.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class index {
 
-    @RequestMapping("/asd")
+    @RequestMapping("/q")
     public String sayHello(){
         return "index";
     }
